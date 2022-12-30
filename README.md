24fc3c311c3465394c84730458c9c8765d3a585b
Muhamed ElSayed <muhamedelsayed@Muhameds-MacBook-Pro.local>
2022-12-30 22:16:27 +0300

    I installed the AnyConnector and connect to our nodes

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 ## Create The docker file to work in container
 
 * I generated the nos_sandbox_testbed yaml file
+* Create script to load nos_sandbox_testbed.yaml and connect to node
+
+Note: You should be follow this steps to move in this model:
+
+1. Go to [https://devnetsandbox.cisco.com/RM/Topology](https://devnetsandbox.cisco.com/RM/Topology) and select ***NSO sandboxbelo***
+2. Install **AnyConnect** from below link:
+   [https://devnetsandbox.cisco.com/Docs/VPN_Access/AnyConnect_Connection_Guide.pdf](https://devnetsandbox.cisco.com/Docs/VPN_Access/AnyConnect_Connection_Guide.pdf)
+3. You will wait 15 min to recive an email with username and password
--- network_inventory.py
+++ network_inventory.py
@@ -2,6 +2,10 @@
 
 import argparse
 from pyats.topology.loader import load
+from pyats.utils.exceptions import SchemaTypeError
+
+def __parse_command__(device, command):
+    print(device, command)
 
 if __name__ == '__main__':
     #-->load pyats testbed file
@@ -15,7 +19,20 @@ if __name__ == '__main__':
     
     #-->Create pyats testbed object
     testbed = load(args.testbed)
+    print(f"Connecting to all devices in testbed {testbed.name}")
+    try:
+        testbed.connect(log_stdout=False)
+    except SchemaTypeError:
+        print(testbed.devices)
 
-    testbed.connect()
 
+    
+    for name in testbed.devices: 
+        print(f"Disconnectingfrom device {name}")
+        __parse_command__(testbed.devices[name], "show version")
+        testbed.devices[name].disconnect()
+
+
+
+    
 
